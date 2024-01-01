88857a935711ac1a4a6b2d7970cac2dbf6984e87
FUSRODAH\minat <nun.odakazu@gmail.com>
2024-01-01 13:39:36 +0900

    特定ページから特定の文字列を探してその結果を返す機能を実装。一日おきに実行する。

---
+++ main.js
@@ -0,0 +1,46 @@
+const puppeteer = require("puppeteer");
+
+const targetURL = "http://gifu-triathlon.jp/event2024.html";
+const targetString = "木曽三川";
+
+const main = async (targetURL, targetString) => {
+  // Puppeteerを起動
+  const browser = await puppeteer.launch();
+
+  // 新しいページを開く
+  const page = await browser.newPage();
+
+  try {
+    // 指定されたURLにアクセス
+    await page.goto(targetURL);
+
+    // ページのコンテンツを取得
+    const pageContent = await page.content();
+
+    // 特定の文字列がページコンテンツに含まれているか確認
+    if (pageContent.includes(targetString)) {
+      console.log(
+        `webページ (o: ${targetURL})内に文字列(${targetString})を発見した。`
+      );
+    } else {
+      console.log(
+        `webページ (x: ${targetURL})内に文字列(${targetString})を発見できなかった。`
+      );
+    }
+  } catch (error) {
+    console.error("エラーが発生しました:", error);
+  } finally {
+    // ブラウザを閉じる
+    await browser.close();
+  }
+};
+
+const interval = 24 * 60 * 60 * 1000; //インターバルは毎日とする
+
+//間隔実行
+setInterval(() => {
+  main(targetURL, targetString);
+}, interval);
+
+//即時実行
+main(targetURL, targetString);
